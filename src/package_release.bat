62ff4fecb32301e62dc438622abdb09569647e1c
SammiLucia <3295286+sammilucia@users.noreply.github.com>
2024-12-14 21:46:09 -0600

    integrate temporal changes

--- src/package_release.bat
+++ src/package_release.bat
@@ -6,12 +6,14 @@ RMDIR /S /Q build
 MKDIR build
 xcopy /s/e/i build_release "build/build_release"
 
-7z a -r "..\dist\Stalker 2 Ultra Plus v%VERSION%.zip" .\build\build_release\*
+DEL ..\dist\*.zip
+pause
+7Z a -r "..\dist\Stalker 2 Ultra Plus v%VERSION%.zip" .\build\build_release\*
 
 RENAME "build/build_release/Stalker2/Binaries/Win64" WinGDK
 
-7z a -r "..\dist\Stalker 2 Ultra Plus v%VERSION%-gp.zip" .\build\build_release\*
-7z a -r "..\dist\Stalker 2 FSR3-FG Addon v%VERSION%.zip" .\build_fsr3-fg\*
+7Z a -r "..\dist\Stalker 2 Ultra Plus v%VERSION%-gp.zip" .\build\build_release\*
+7Z a -r "..\dist\Stalker 2 FSR3-FG Addon v%VERSION%.zip" .\build_fsr3-fg\*
 
 echo.
 
